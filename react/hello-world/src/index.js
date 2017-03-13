4887302ae955ab41133fecb8d99a6745f895f7d6
Marcio Bera <marciobera@hotmail.com>
2017-03-13 20:29:39 -0300

    page 'Hello World' from @react docs

--- react/hello-world/src/index.js
+++ react/hello-world/src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
 ReactDOM.render(
-  <App />,
+  <h1>Hello, world!</h1>,
   document.getElementById('root')
 );
