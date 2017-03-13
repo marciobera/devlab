892621d2364ce6c02340314884754075ac9242b7
Marcio Bera <marciobera@hotmail.com>
2017-03-13 20:36:59 -0300

    page 'Introducing JSX' from @react docs

--- react/hello-world/src/index.js
+++ react/hello-world/src/index.js
@@ -8,7 +8,22 @@ import './index.css';
 //   document.getElementById('root')
 // );
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Marcio',
+  lastName: 'Bera'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  element,
   document.getElementById('root')
 );
