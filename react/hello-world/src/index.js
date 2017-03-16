502c84aa9d7ce22d30d9004ed67ce0bb195c2a48
Marcio Bera <marciobera@hotmail.com>
2017-03-16 00:11:53 -0300

    EX 2: Components and Props in @react docs

--- react/hello-world/src/index.js
+++ react/hello-world/src/index.js
@@ -1,53 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import './index.css';
 
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
-
-// function formatName(user) {
-//   return user.firstName + ' ' + user.lastName;
-// }
-
-// const user = {
-//   firstName: 'Marcio',
-//   lastName: 'Bera'
-// };
-
-
-// const element = React.createElement(
-//   'h1',
-//   {className: 'greeting'},
-//   'Hello, ' + formatName(user)
-// );
-
-// ReactDOM.render(
-//   element,
-//   document.getElementById('root')
-// );
-
-// function tick() {
-//   const element = (
-//     <div>
-//       <h1>Hello, world!</h1>
-//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
-//     </div>
-//   );
-//   ReactDOM.render(
-//     element,
-//     document.getElementById('root')
-//   );
-// }
-
-// setInterval(tick, 1000);
 function Welcome(props) {
   return <h1>Hello, {props.name}</h1>;
 }
-const element = <Welcome name="Sara" />;
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
+
 ReactDOM.render(
-  element,
+  <App />,
   document.getElementById('root')
 );
