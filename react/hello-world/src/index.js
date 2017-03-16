5d80e9a50684ef07106cb7ef1b5e1723618da0eb
Marcio Bera <marciobera@hotmail.com>
2017-03-16 00:26:20 -0300

    Ex 3: Components and Props of @react docs.

--- react/hello-world/src/index.js
+++ react/hello-world/src/index.js
@@ -3,21 +3,55 @@ import ReactDOM from 'react-dom';
 // import App from './App';
 import './index.css';
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+function formatDate(date) {
+  return date.toLocaleDateString();
 }
 
-function App() {
+function Avatar(props) {
   return (
-    <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
+    <img className="Avatar"
+         src={props.user.avatarUrl}
+         alt={props.user.name} />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
     </div>
   );
 }
 
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+};
 ReactDOM.render(
-  <App />,
+  <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author} />,
   document.getElementById('root')
 );
