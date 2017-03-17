9d72fc1a23e4526d033f95115a94e1643bac3c12
Marcio Bera <marciobera@hotmail.com>
2017-03-17 01:22:01 -0300

    Finish read  page, from @react docs.

--- react/hello-world/src/index.js
+++ react/hello-world/src/index.js
@@ -3,23 +3,54 @@ import ReactDOM from 'react-dom';
 // import App from './App';
 import './index.css';
 
+function FormattedDate(props) {
+  return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+}
+
 class Clock extends React.Component {
   constructor(props) {
     super(props);
     this.state = {date: new Date()};
   }
 
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <FormattedDate date={this.state.date} />
       </div>
     );
   }
 }
 
+function App() {
+  return (
+    <div>
+      <Clock />
+      <Clock />
+      <Clock />
+    </div>
+  );
+}
+
 ReactDOM.render(
-  <Clock />,
+  <App />,
   document.getElementById('root')
 );
