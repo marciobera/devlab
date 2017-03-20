90287b76f2ad4f3a4e9208622360c6d0fee8ba31
Marcio Bera <marciobera@hotmail.com>
2017-03-19 22:57:48 -0300

    'Keys Must Only Be Unique Among Siblings' example of @react docs

--- react/hello-world/src/index.js
+++ react/hello-world/src/index.js
@@ -3,20 +3,36 @@ import ReactDOM from 'react-dom';
 // import App from './App';
 import './index.css';
 
-function NumberList(props) {
-  const numbers = props.numbers;
-  const listItems = numbers.map((number) =>
-    <li key={number.toString()}>
-      {number}
-    </li>
+function Blog(props) {
+  const sidebar = (
+    <ul>
+      {props.posts.map((post) =>
+        <li key={post.id}>
+          {post.title}
+        </li>
+      )}
+    </ul>
+  );
+  const content = props.posts.map((post) =>
+    <div key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
   );
   return (
-    <ul>{listItems}</ul>
+    <div>
+      {sidebar}
+      <hr />
+      {content}
+    </div>
   );
 }
 
-const numbers = [1, 2, 3, 4, 5];
+const posts = [
+  {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+  {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+];
 ReactDOM.render(
-  <NumberList numbers={numbers} />,
+  <Blog posts={posts} />,
   document.getElementById('root')
 );
