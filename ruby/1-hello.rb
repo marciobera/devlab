21fc90fb1927e946b9aa3e88c5f7a8740a829f25
Marcio Bera <marciobera@hotmail.com>
2017-04-03 16:45:43 -0300

    1.2 Get the type of a variable class

--- ruby/1-hello.rb
+++ ruby/1-hello.rb
@@ -1,7 +1,20 @@
+=begin
+
+1. Introduction
+
+=end
+
 idade = 27
 multiplicador = 2
 
 #puts idade * multiplicador
+
+
+=begin
+
+1.1 Set the def 'plural' in the String's class
+
+=end
 
 class String
 	def plural
@@ -9,4 +22,14 @@ class String
 	end
 end
 
-puts "caneta".plural
+#puts "caneta".plural
+
+
+
+=begin
+
+1.2 Get the type of a variable class
+
+=end
+
+puts idade.class
