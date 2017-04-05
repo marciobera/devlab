9999446644cd1a63fc678638dd476b81f00aae27
Marcio Bera <marciobera@hotmail.com>
2017-04-05 15:40:07 -0300

    Chapter 2.8 - Simple Interactions with for, while and until

---
+++ ruby/chapter_2.8.rb
@@ -0,0 +1,18 @@
+#FOR
+for number in (1..100)
+	puts "Number: #{number} - for"
+end
+
+#WHILTE
+number  = 0
+while number <= 100
+	puts "Number: #{number} - while"
+	number += 1
+end
+
+#UNTIL
+number = 0
+until number == 100
+	puts "Number: #{number} - until"
+	number += 1
+end
