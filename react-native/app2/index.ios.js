fc4e1f338755d1d66878a01b302c9d7b2fbe061d
Marcio Bera <marciobera@hotmail.com>
2017-08-08 02:00:22 -0300

    Working with button (style) - pt2

--- react-native/app2/index.ios.js
+++ react-native/app2/index.ios.js
@@ -1,27 +1,42 @@
 //Import
 import React from 'react';
-import { Text, AppRegistry, View, Button } from 'react-native';
+import { Text, AppRegistry, View, TouchableOpacity } from 'react-native';
 
 //Formatações
 const Estilos = {
 	principal: {
 		paddingTop:40
+	},
+	botao: {
+		backgroundColor: '#48BBEC',
+		padding: 10,
+		borderColor: '#1d8eb8',
+		borderWidth: 1,
+		borderRadius: 8,
+		margin: 5,
+		shadowColor: '#000',
+		shadowOffset: {width: 0, height: 2},
+		shadowOpacity: 0.4
+	},
+	textoBotao: {
+		color: '#fff',
+		fontSize: 16,
+		fontWeight: 'bold',
+		alignSelf: 'center'
 	}
+
 };
 const botaoPressionado = () => {
 	alert('Botão pressionado!');
 };
 //Criar o componente
 const App = () => {
-	const { principal } = Estilos;
+	const { principal, botao, textoBotao } = Estilos;
   return (
   	<View style={ principal } >
-      <Button 
-      	onPress={botaoPressionado}
-      	title="Clique aqui"
-      	color="#841584"
-      	acessabilityLabel="Clique aqui para abrir as notícias"
-      /> 
+  		<TouchableOpacity style={botao}>
+  			<Text style={textoBotao}>Clique aqui</Text> 
+  		</TouchableOpacity>
     </View>
     );
 };
