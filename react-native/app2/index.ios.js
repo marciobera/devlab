d84d8c73d124d8e3379a490dcbbf3131523040d1
Marcio Bera <marciobera@hotmail.com>
2017-08-08 02:32:19 -0300

    Creating the App logical. Finishing the First completely App.

--- react-native/app2/index.ios.js
+++ react-native/app2/index.ios.js
@@ -1,6 +1,6 @@
 //Import
 import React from 'react';
-import { Text, AppRegistry, View, Image, TouchableOpacity } from 'react-native';
+import { Text, AppRegistry, View, Image, TouchableOpacity, Alert } from 'react-native';
 
 //Formatações
 const Estilos = {
@@ -22,8 +22,31 @@ const Estilos = {
 	}
 };
 
-const botaoPressionado = () => {
+const gerarNovaFrase = () => {
+	var numeroAleatorio = Math.random();
+	numeroAleatorio = Math.floor(numeroAleatorio * 16);
 
+	var frases = Array();
+	frases.push('Se a caminhada está difícil, é porque você está no caminho certo.');
+	frases.push('Seja humilde para admitir seus erros, inteligente para aprender com eles e maduro para corrigi-los.');
+	frases.push('Seja você mesmo, todos os outros já existem.');
+	frases.push('Se eu não mudar o que faço hoje, todos os amanhãs serão iguais a ontem.');
+	frases.push('Eu ainda não cheguei lá, mas estou mais perto do que ontem.');
+	frases.push('Superar não é escolha, é necessidade.');
+	frases.push('Que a vontade de ser feliz seja sempre maior que o medo de se machucar.');
+	frases.push('Seja a mudança que você quer ver no mundo.');
+	frases.push('O seu sorriso pode mudar o dia de alguém.');
+	frases.push('Nunca deixe de lutar por medo de errar ou de se machucar. As feridas com o tempo se curam, mas as oportunidades não voltam.');
+	frases.push('Não há dias cinzentos para quem sonha colorido.');
+	frases.push('Não fique esperando o futuro para ser feliz, faça do presente a sua alegria.');
+	frases.push('Colecione momentos, não coisas.');
+	frases.push('Comece onde você está, use o que você tem e faça o que você pode.');
+	frases.push('Você não é derrotado quando perde. Você é derrotado quando desiste.');
+	frases.push('Viver não é esperar a tempestade passar. É aprender a dançar na chuva.');
+
+	var fraseEscolhida = frases[numeroAleatorio];
+
+	Alert.alert(fraseEscolhida);
 }
 
 //Criar o componente
@@ -33,7 +56,10 @@ const App = () => {
   	<View style={ principal } >
   		<Image source={ require('./imgs/logo.png') } />
   		
-  		<TouchableOpacity style={ botao }>
+  		<TouchableOpacity
+  			onPress={gerarNovaFrase}
+  			style={ botao }
+  			>
   			<Text style={ textoBotao} >Nova frase</Text>
   		</TouchableOpacity>
     </View>
