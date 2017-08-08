d48c864a5a8d11d36a824d988df652317a493eda
Marcio Bera <marciobera@hotmail.com>
2017-08-08 01:13:06 -0300

    Applying effects in text.

--- react-native/app2/index.ios.js
+++ react-native/app2/index.ios.js
@@ -5,7 +5,12 @@ import { Text, AppRegistry } from 'react-native';
 //Formatações
 const Estilos = {
 	estiloTexto: {
-		fontSize: 50
+		fontSize: 30,
+		backgroundColor: '#08509B',
+		color: '#fff',
+		paddingLeft: 10,
+		paddingBottom: 5,
+		paddingTop: 20,
 	}
 };
 
