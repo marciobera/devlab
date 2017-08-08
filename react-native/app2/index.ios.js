f133c3aa3a38086af956ea607f42090c57b7d4b6
Marcio Bera <marciobera@hotmail.com>
2017-08-08 01:36:22 -0300

    Applying flexbox align - pt 2

--- react-native/app2/index.ios.js
+++ react-native/app2/index.ios.js
@@ -8,24 +8,32 @@ const Estilos = {
 		fontSize: 40,
 		backgroundColor: '#08509B',
 		height: 60,
-		width:60
+
+	},
+	estiloTexto2: {
+		fontSize: 40,
+		backgroundColor: '#2A48FA',
+		height: 60
 
 	},
 	estiloView: {
 		backgroundColor: 'skyblue',
-		height:300,
+		height: '100%',
 		//flex-start, center, flex-end
-		justifyContent: 'center',
-		alignItems: 'center'
+		justifyContent: 'space-around',
+		alignItems: 'stretch',
+		flexDirection: 'column'
 	}
 };
 
 //Criar o componente
 const App = () => {
-	const { estiloTexto, estiloView } = Estilos;
+	const { estiloTexto, estiloTexto2, estiloView } = Estilos;
   return (
   	<View style={ estiloView } >
-      <Text style={ Estilos.estiloTexto }></Text>
+      <Text style={ Estilos.estiloTexto }>A</Text>
+      <Text style={ Estilos.estiloTexto2 }>B</Text>
+      <Text style={ Estilos.estiloTexto }>C</Text>
     </View>
     );
 };
