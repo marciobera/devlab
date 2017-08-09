12d7eafdc1f2a705a261675a0c506a1f206b9d02
Marcio Bera <marciobera@hotmail.com>
2017-08-09 01:06:49 -0300

    Working with states.

--- react-native/app3/index.ios.js
+++ react-native/app3/index.ios.js
@@ -9,16 +9,15 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
 class MeuComponente extends Component{
   render(){
     return (
       <View>
-        <Text>{this.props.propriedade1}</Text>
-        <Text>{this.props.xyz}</Text>
-        <Text>{this.props.p}</Text>
+        <Text>{this.props.teste}</Text>
       </View>
       );
   }
@@ -26,10 +25,25 @@ class MeuComponente extends Component{
 
 class app3 extends Component{
 
+  constructor(props){
+    super(props);
+
+    this.state = {texto: 'Texto teste 2'};
+  }
+
+  alteraTexto(){
+    this.setState({texto: 'Outra coisa'});
+  }
 
   render(){
     return (
-      <MeuComponente propriedade1='Banana' xyz='Abacaxi' p='Uva'></MeuComponente>
+      <View>
+        <MeuComponente teste={this.state.texto}></MeuComponente>
+        <Button
+          title='Botão'
+          onPress={() => { this.alteraTexto()} }
+        />
+      </View>
       );
   }
 
