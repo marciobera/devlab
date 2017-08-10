e625e9f0181a8397a7d3e685884ce0e03e7c4a2d
Marcio Bera <marciobera@hotmail.com>
2017-08-10 01:19:42 -0300

    Getting data via http using 'axios'

--- react-native/app4/src/components/ListaItens.js
+++ react-native/app4/src/components/ListaItens.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import {
-  View
+  ScrollView,
+  Text
 } from 'react-native';
+import axios from 'axios';
 import Itens from './Itens';
 
 export default class ListaItens extends Component {
 
+	constructor(props) {
+		super(props);
+		
+		this.state = { listaItens: [] };
+	}
+
+	componentWillMount() {
+		//requisição http
+		axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+			.then(response => { this.setState( { listaItens: response.data } ); })
+			.catch(() => { console.log('Erro ao recuperar os dados'); });
+	
+	}
+
   render() {
     return (
-      <View>
-      	<Itens />
-      	<Itens />
-      	<Itens />
-      </View>
+      <ScrollView>
+      	{ this.state.listaItens.map(item => (<Itens key={item.titulo} item={item} />) ) }
+      </ScrollView>
     );
   }
 }
