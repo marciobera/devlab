f2e8a096c3edc7966b35c51abc414c7d9f496e9e
Marcio Bera <marciobera@hotmail.com>
2017-08-10 01:39:57 -0300

    Defining style in app4.

--- react-native/app4/src/components/ListaItens.js
+++ react-native/app4/src/components/ListaItens.js
@@ -24,7 +24,7 @@ export default class ListaItens extends Component {
 
   render() {
     return (
-      <ScrollView>
+      <ScrollView style={{ backgroundColor: '#DDD', paddingTop: 20 }}>
       	{ this.state.listaItens.map(item => (<Itens key={item.titulo} item={item} />) ) }
       </ScrollView>
     );
