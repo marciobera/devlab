e71bf34d4f5bfb3dfb9b979a2910c88bb7a170d6
Marcio Bera <marciobera@hotmail.com>
2017-08-10 00:39:15 -0300

    Starting a new app 'app4'.

---
+++ react-native/app4/src/components/Itens.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+  Text
+} from 'react-native';
+
+export default class Itens extends Component {
+
+  render() {
+    return (
+      <Text>
+      	Detalhes do Item
+      </Text>
+    );
+  }
+}
---
+++ react-native/app4/src/components/ListaItens.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  View
+} from 'react-native';
+import Itens from './Itens';
+
+export default class ListaItens extends Component {
+
+  render() {
+    return (
+      <View>
+      	<Itens />
+      	<Itens />
+      	<Itens />
+      </View>
+    );
+  }
+}
