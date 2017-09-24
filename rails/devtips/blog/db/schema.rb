5a3c18422c52f6f7daebbb8666d5938b3ec9c7c5
Marcio Bera <marciobera@hotmail.com>
2017-09-24 08:09:48 -0300

    Generating module with scaffold.

--- rails/devtips/blog/db/schema.rb
+++ rails/devtips/blog/db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170924024148) do
+ActiveRecord::Schema.define(version: 20170924042417) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "todos", force: :cascade do |t|
+    t.string "title"
+    t.text "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
