9903bcb117852d8ae0f348692c477cd219afa9ad
Marcio Bera <marciobera@hotmail.com>
2017-11-30 23:59:15 -0200

    Getting Started with the Firebase Realtime Database PT2.

--- firebase/public/app_rdb.js
+++ firebase/public/app_rdb.js
@@ -3,20 +3,43 @@
 
 	// Initialize Firebase
  	var config = {
-
 	};
 	firebase.initializeApp(config);
 
 
 	// Get elements
 	const preObject = document.getElementById('object');
+	const ulList = document.getElementById('list');
 
 	// Create references
 	const dbRefObject = firebase.database().ref().child('object');
+	const dbRefList = dbRefObject.child('hobbies');
 
 	// Sync object changes
 	dbRefObject.on('value', snap => {
 		preObject.innerText = JSON.stringify(snap.val(), null, 3);
+	});
+
+	// Sync list changes
+	dbRefList.on('child_added', snap => {
+		const li = document.createElement('li');
+		li.innerText = snap.val();
+		li.id = snap.key;
+		ulList.appendChild(li);
+	});
+
+	dbRefList.on('child_changed', snap => {
+		
+		const liChanged = document.getElementById(snap.key);
+		liChanged.innerText = snap.val();
+
+	});
+
+	dbRefList.on('child_removed', snap => {
+		
+		const liToRemove = document.getElementById(snap.key);
+		liToRemove.remove();
+
 	});
 	
 }());
