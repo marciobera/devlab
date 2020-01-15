7f9be036f196ec2d8df39884d80795faa653f15c
Marcio Bera <marciobera@hotmail.com>
2020-01-15 21:56:22 +0000

    Test a function: using multiple assertions

---
+++ phpunit/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @param integer $a The first number
+ * @param integer $b The second number
+ * 
+ * @return integer The sum of two numbers
+ */
+function add($a, $b) {
+    return $a + $b;
+}
