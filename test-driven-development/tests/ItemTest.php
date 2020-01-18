a499e5c1493b85cea83a31971269bd2406e657be
Marcio Bera <marciobera@hotmail.com>
2020-01-18 23:17:46 +0000

    Testing private methods with arguments using reflection

--- test-driven-development/tests/ItemTest.php
+++ test-driven-development/tests/ItemTest.php
@@ -23,11 +23,24 @@ class ItemTest extends TestCase
     public function testTokenIsAString()
     {
         $item = new Item;
+        
         $reflector = new ReflectionClass(Item::class);
         $method = $reflector->getMethod('getToken');
         $method->setAccessible(true);
         $result = $method->invoke($item);
 
         $this->assertIsString($result);
+    }
+
+    public function testPrefixedTokenStartsWithPrefix()
+    {
+        $item = new Item;
+        
+        $reflector = new ReflectionClass(Item::class);
+        $method = $reflector->getMethod('getPrefixedToken');
+        $method->setAccessible(true);
+        $result = $method->invokeArgs($item, ['example']);
+
+        $this->assertStringStartsWith('example', $result); 
     }
 }
