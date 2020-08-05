ac3d928529a56a77e738b35cd0bcde4fcedc1ca5
Marcio Bera <marciobera@hotmail.com>
2020-08-05 21:25:02 +0100

    NLW #2 Backend-Frontend integration

--- nlw-02/aulas/web/src/components/TeacherItem/index.tsx
+++ nlw-02/aulas/web/src/components/TeacherItem/index.tsx
@@ -2,37 +2,54 @@ import React from "react";
 
 import "./styles.css";
 import whastappIcon from "../../assets/images/icons/whatsapp.svg";
+import api from "../../services/api";
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  subject: string;
+  cost: string;
+  name: string;
+  avatar: string;
+  whatsapp: string;
+  bio: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post("connections", {
+      user_id: teacher.id,
+    });
+  }
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars0.githubusercontent.com/u/7188397?s=460&u=31cc3c61a4c3a8a156b9f6dd52a01d38df40fd3c&v=4" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Marcio Bera</strong>
-          <span>Informática</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Entusiasta das melhores tecnologicas de quimica avançada.
-        <br />
-        <br />
-        Apaixonado por explodir coisas em laboratórios e por mudar a vida das
-        pessoas através de experiencias.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+          target="_blank"
+        >
           <img src={whastappIcon} alt="WhatsApp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
-}
+};
 
 export default TeacherItem;
