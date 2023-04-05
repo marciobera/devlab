fda8b7fe2d9169fbc3f162d32fff28e7fbcd118f
Marcio Bera <marciobera@hotmail.com>
2023-04-05 17:26:16 +0100

    Add fallback to open on the primary monitor

--- electron/my-electron-app/index.js
+++ electron/my-electron-app/index.js
@@ -1,19 +1,25 @@
 const { app, BrowserWindow, screen } = require("electron");
 
 const createWindow = () => {
-  // const primaryDisplay = screen.getPrimaryDisplay();
-  // const { width, height } = primaryDisplay.workAreaSize;
-
   const displays = screen.getAllDisplays();
   const externalDisplay = displays.find((display) => {
     return display.bounds.x !== 0 || display.bounds.y !== 0;
   });
 
   if (externalDisplay) {
+    // Open on the secondary monitor
     win = new BrowserWindow({
       x: externalDisplay.bounds.x + 0,
       y: externalDisplay.bounds.y + 0,
       fullscreen: true,
+    });
+  } else {
+    // Open on the primary monitor
+    const primaryDisplay = screen.getPrimaryDisplay();
+    const { width, height } = primaryDisplay.workAreaSize;
+    const win = new BrowserWindow({
+      width,
+      height,
     });
   }
 
